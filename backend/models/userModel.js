818917b1f240ac3f56a87a14a8bb78a42df33a5c
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-20 20:33:43 +0530

    Added the user model file

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { isLowercase, trim } from "validator";
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+
+    password:{
+        type:String,
+        required:true   
+    }
+
+}, {timestamps:true})
+
+export default mongoose.model.uSer || mongoose.model('User', userSchema);
