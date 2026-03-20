a95497a13a8adca6b4011519dbe821ba5dd34eae
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-20 20:29:30 +0530

    Add the Database connection

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("✅ DB CONNECTED");
+    } catch (error) {
+        console.error("❌ DB CONNECTION FAILED:", error.message);
+        process.exit(1); 
+    }
+};
