a8865247c16c5fafbdf1cbecc5f5310b4bca50ad
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-20 20:34:24 +0530

    Server file

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+import { connectDB } from "./config/db.js";
+
+const app = express();
+const port = 4000;
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Routes
+
+app.get("/", (req,res)=>{
+    res.send("API working ");
+})
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
+    connectDB();
+})
