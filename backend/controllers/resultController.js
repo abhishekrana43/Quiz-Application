6d1038768cb51ff24bc69f957dfb9bcd9d0b2de1
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-22 13:19:08 +0530

    Added createResult

---
+++ backend/controllers/resultController.js
@@ -0,0 +1,57 @@
+import Result from "../models/resultModel.js";
+
+export async function createResult(req,res) {
+    try {
+        if(!req.user || !req.user._id){
+            return res.status(401).json({
+                success:false,
+                message:"Not authorized"
+            })
+        }
+
+        const {title, technology,level,  totalQuestions, correct, wrong} = req.body;
+        if(!technology || !level || totalQuestions === undefined || correct === undefined){
+            return res.status(400).json({
+                success:false,
+                message:"Missing fields"
+            });
+
+        }
+
+        const computedWrong = wrong !== undefined ? Number(wrong): Math.max(0, Number(totalQuestions) - Number(correct));
+
+        if(!title){
+         return res.status(400).json({
+            success:false,
+            message:"title is missing"
+         })  
+        };
+
+     const payload = {
+      title: String(title).trim(),
+      technology,
+      level,
+      totalQuestions: Number(totalQuestions),
+      correct: Number(correct),
+      wrong: computedWrong,
+      user: req.user.id // for a particular user
+    };
+
+    const created = await Result.create(payload);
+    return res.status(201).json({
+        success:true,
+        message:"Result Created"
+    })
+    } catch (error) {
+    console.error("CreatedResult Error", err);
+    return res.status(500).json({
+        success:false,
+        message:"Server Error"
+    }) 
+    }
+}
+
+// LIST THE RESULT
+export async function listResult(params) {
+    
+}
