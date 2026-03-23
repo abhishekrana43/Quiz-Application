e5f88799fad23d778d9884d635482c3da98500d8
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-23 22:17:19 +0530

    Server.js updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import 'dotenv/config'
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
+import resultRouter from "./routes/resultRoutes.js";
 
 const app = express();
 const port = 4000;
@@ -15,6 +16,7 @@ app.use(express.urlencoded({extended:true}))
 //Routes
 
 app.use('/api/auth', userRouter);
+app.use('/api/results', resultRouter);
 
 app.get("/", (req,res)=>{
     res.send("API working ");
