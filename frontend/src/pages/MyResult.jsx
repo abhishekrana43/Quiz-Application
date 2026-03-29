edcfee97ed11ea58cf4d22825e7dc339e47a4014
Abhishek Rana <abhishekrana0743@gmail.com>
2026-03-29 22:28:08 +0530

    Pages added

---
+++ frontend/src/pages/MyResult.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import MyResult from '../components/MyResult'
+
+export default function MyResult() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
